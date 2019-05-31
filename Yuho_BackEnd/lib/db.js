41a88973beebf48994a7c2a57b080f52113d89c5
yuho8437 <yuho8437@unist.ac.kr>
2019-06-01 00:08:08 +0900

    db update

--- Yuho_BackEnd/lib/db.js
+++ Yuho_BackEnd/lib/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 /* MySQL method 가져오기 */
 var db = mysql.createConnection({
-    host: 'localhost',       // 서버위치
-    user: 'newUser',         // 유저 
-    password: '111111',      // 비밀번호
+    host: '35.187.232.156',   // 서버위치
+    user: 'DTS',         // 유저 
+    password: '971004',      // 비밀번호
     database: 'opentutorials'// 사용하려는 데이터베이스
 });
 /* 실제 접속 */
